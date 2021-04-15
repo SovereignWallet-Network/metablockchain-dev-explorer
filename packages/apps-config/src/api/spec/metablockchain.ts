d09e71359a20e97c7a9b0da1bb2a418f2a988344
Ayush <ayushmishra2005@gmail.com>
2021-04-15 17:30:40 +0530

    Updated properties according to new DID changes

--- packages/apps-config/src/api/spec/metablockchain.ts
+++ packages/apps-config/src/api/spec/metablockchain.ts
@@ -1,23 +1,43 @@
 export default {
-  PeerId: 'Vec<u8>',
-  identifier: '[u8;32]',
-  public_key: '[u8;32]',
-  metadata: 'Vec<u8>',
-  DidStruct: {
-    identifier: 'identifier',
-    public_key: 'public_key',
-    metadata: 'metadata'
-  },
-  Did: '[u8;32]',
-  PublicKey: '[u8;32]',
-  Address: 'MultiAddress',
-  LookupSource: 'MultiAddress',
-  TreasuryProposal: {
-    proposer: 'Did',
-    beneficiary: 'Did',
-    value: 'Balance',
-    bond: 'Balance'
-  },
-  CurrencyId: 'i64',
-  Amount: 'i64'
+        "PeerId": "(Vec<>)",
+          "identifier": "[u8;32]",
+          "public_key": "[u8;32]",
+          "metadata": "Vec<u8>",
+          "issuer": "Option<[u8;32]>",
+          "DidStruct": {
+            "identifier": "identifier",
+            "public_key": "public_key",
+            "metadata": "metadata",
+            "issuer": "issuer"
+          },
+          "DiDSignature": "[u8;64]",
+         "VcTYPE": {
+           "_enum": [
+             "Add",
+             "Update",
+             "Remove",
+             "Rotate"
+           ]
+         },
+        "Did": "[u8;32]",
+          "VcProperty": {
+            "issuer": "Did",
+            "vc_type": "VcTYPE"
+          },
+          "VcProperties": {
+            "property": "VcProperty",
+            "hash": "Hash",
+            "signature": "DiDSignature"
+          },
+          "PublicKey": "[u8;32]",
+          "Address": "MultiAddress",
+          "LookupSource": "MultiAddress",
+          "TreasuryProposal": {
+            "proposer": "Did",
+            "beneficiary": "Did",
+            "value": "Balance",
+            "bond": "Balance"
+          },
+          "CurrencyId": "i64",
+          "Amount": "i64"
 };
